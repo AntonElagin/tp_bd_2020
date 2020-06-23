c1df505b0e5656e741b4180feb6bbb9a1f506cf7
AntonElagin <anton120299@gmail.com>
2020-06-23 07:56:20 +0300

    add express-cluster

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 
-const app = express();
+process.env.NODE_ENV = 'production';
 
 const userRouter = require('./routes/userRouter');
 const forumRouter = require('./routes/forumRouter');
@@ -9,17 +9,24 @@ const threadRouter = require('./routes/threadRouter');
 const postRouter = require('./routes/postRouter');
 const serviceRouter = require('./routes/serviceRouter');
 
-app.use(logger('dev'));
-app.use(express.json());
 
-app.use('/api/user', userRouter);
-app.use('/api/forum', forumRouter);
-app.use('/api/thread', threadRouter);
-app.use('/api/post', postRouter);
-app.use('/api/service', serviceRouter);
+const cluster = require('express-cluster');
 
-const port = process.env.PORT || 5000;
-app.listen(
-    port,
-    () => console.log('Forum API server is running on port: ', port),
-);
+cluster(function(worker) {
+  const app = express();
+
+  //   app.use(logger('dev'));
+  app.use(express.json());
+
+  app.use('/api/user', userRouter);
+  app.use('/api/forum', forumRouter);
+  app.use('/api/thread', threadRouter);
+  app.use('/api/post', postRouter);
+  app.use('/api/service', serviceRouter);
+
+  const port = process.env.PORT || 5000;
+  return app.listen(
+      port,
+      () => console.log('Forum API server is running on port: ', port),
+  );
+}, {respawn: false});

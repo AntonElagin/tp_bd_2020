99f1d5bdd3acdc6e732cfe02d4bd26cf5f36c982
AntonElagin <anton120299@gmail.com>
2020-06-24 13:47:29 +0300

    fastify cluster

--- src/server.js
+++ src/server.js
@@ -12,37 +12,35 @@ const serviceRouter = require('./routes/serviceRouter');
 
 const cluster = require('express-cluster');
 
-// cluster(function(worker) {
-const fastify = lib({
-  logger: true,
-});
+cluster(function(worker) {
+  const fastify = lib();
 
-fastify.addContentTypeParser('application/json',
-    {parseAs: 'string'}, function(req, body, done) {
-      try {
-        if (body) {
-          const json = JSON.parse(body);
-          done(null, json);
-        } else {
-          done(null, {});
+  fastify.addContentTypeParser('application/json',
+      {parseAs: 'string'}, function(req, body, done) {
+        try {
+          if (body) {
+            const json = JSON.parse(body);
+            done(null, json);
+          } else {
+            done(null, {});
+          }
+        } catch (err) {
+          err.statusCode = 400;
+          done(err, undefined);
         }
-      } catch (err) {
-        err.statusCode = 400;
-        done(err, undefined);
-      }
-    });
-fastify.register(userRouter, {prefix: '/api/user'});
-fastify.register(forumRouter, {prefix: '/api/forum'});
-fastify.register(threadRouter, {prefix: '/api/thread'});
-fastify.register(postRouter, {prefix: '/api/post'});
-fastify.register(serviceRouter, {prefix: '/api/service'});
+      });
+  fastify.register(userRouter, {prefix: '/api/user'});
+  fastify.register(forumRouter, {prefix: '/api/forum'});
+  fastify.register(threadRouter, {prefix: '/api/thread'});
+  fastify.register(postRouter, {prefix: '/api/post'});
+  fastify.register(serviceRouter, {prefix: '/api/service'});
 
-const port = process.env.PORT || 5000;
-return fastify.listen(
-    port, '0.0.0.0',
-    (err, address) => {
-      if (err) throw err;
-      console.log('Forum API server is running on port: ', address);
-    },
-);
-// }, {respawn: true});
+  const port = process.env.PORT || 5000;
+  return fastify.listen(
+      port, '0.0.0.0',
+      (err, address) => {
+        if (err) throw err;
+        console.log('Forum API server is running on port: ', address);
+      },
+  );
+}, {respawn: true});
